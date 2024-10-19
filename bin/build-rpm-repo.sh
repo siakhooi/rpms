bc331420b3873fac1ae07b9b4fb2498e47c23548
Siak Hooi <siakhooi@gmail.com>
2024-10-19 09:53:44 +0000

    fix indent 2 spaces

--- bin/build-rpm-repo.sh
+++ bin/build-rpm-repo.sh
@@ -4,12 +4,12 @@ set -ex
 
 cd docs && (
 
-    GNUPGHOME="$(mktemp -d)"
-    export GNUPGHOME
-    echo "$GPG_PRIVATE_KEY" | gpg --import
-    gpg --list-keys
+  GNUPGHOME="$(mktemp -d)"
+  export GNUPGHOME
+  echo "$GPG_PRIVATE_KEY" | gpg --import
+  gpg --list-keys
 
-    find . -name '*.rpm' -exec rpmsign -D "_gpg_name $GPG_KEY_NAME" --addsign {} \;
+  find . -name '*.rpm' -exec rpmsign -D "_gpg_name $GPG_KEY_NAME" --addsign {} \;
 
-    createrepo .
+  createrepo .
 )
